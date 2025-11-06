e3f32760d59b5ba0c1a684d7c6f52ea20e967616
remigiuszk <remigiusz20kaminski@gmail.com>
2025-11-06 13:11:15 +0100

    test

--- hooks/useInitializeApp.js
+++ hooks/useInitializeApp.js
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import * as ScreenOrientation from "expo-screen-orientation";
-import * as NavigationBar from "expo-navigation-bar";
 import * as Font from "expo-font";
 
 export default function useInitializeApp() {
