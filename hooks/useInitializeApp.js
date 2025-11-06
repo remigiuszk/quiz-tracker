919cca0a10aa9046ee4d2f5e0a0689fa1acf8ef1
remigiuszk <remigiusz20kaminski@gmail.com>
2025-11-06 13:08:38 +0100

    test

--- hooks/useInitializeApp.js
+++ hooks/useInitializeApp.js
@@ -4,28 +4,11 @@ import * as NavigationBar from "expo-navigation-bar";
 import * as Font from "expo-font";
 
 export default function useInitializeApp() {
-  const [orientation, setOrientation] = useState(1);
   const [fontLoaded, setFontLoaded] = useState(false);
 
   useEffect(() => {
-    lockOrientation();
     loadFont();
   }, []);
-
-  const lockOrientation = async () => {
-    await ScreenOrientation.lockAsync(
-      ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT
-    );
-    const o = await ScreenOrientation.getOrientationAsync();
-    setOrientation(o);
-  };
-
-  const initLayout = () => {
-    NavigationBar.setPositionAsync("relative");
-    NavigationBar.setVisibilityAsync("hidden");
-    NavigationBar.setBehaviorAsync("inset-swipe");
-    NavigationBar.setBackgroundColorAsync("black");
-  };
 
   const loadFont = async () => {
     await Font.loadAsync({
@@ -34,5 +17,5 @@ export default function useInitializeApp() {
     setFontLoaded(true);
   };
 
-  return { orientation, fontLoaded };
+  return { fontLoaded };
 }
