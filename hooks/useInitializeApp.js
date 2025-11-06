94656fe2fd7bf8a396244c81d401970b68caec28
remigiuszk <remigiusz20kaminski@gmail.com>
2025-11-06 13:20:26 +0100

    test

--- hooks/useInitializeApp.js
+++ hooks/useInitializeApp.js
@@ -1,11 +1,23 @@
 import { useState, useEffect } from "react";
 import * as Font from "expo-font";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { Platform } from "react-native";
 
 export default function useInitializeApp() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
   useEffect(() => {
-    loadFont();
+    (async () => {
+      // ANDROID: wymuś landscape na starcie (działa w Expo Go i w buildach)
+      if (Platform.OS === "android") {
+        try {
+          await ScreenOrientation.lockAsync(
+            ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT
+          );
+        } catch {}
+      }
+      await loadFont();
+    })();
   }, []);
 
   const loadFont = async () => {
